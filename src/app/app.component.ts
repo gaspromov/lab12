0212f256c5d7293c9abdd1cdb168d66f52780771
Gaspromov <gaspromov@mail.ru>
2019-12-27 02:42:03 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Person } from './shared/models/person.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Компоненты';
+  persons: Person[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.persons.push(new Person('Ivan', 'Ivanov', 1));
+    this.persons.push(new Person('Ivan', 'Ivanov', 2));
+    this.persons.push(new Person('Ivan', 'Ivanov', 3));
+    this.persons.push(new Person('Ivan', 'Ivanov', 4));
+    this.persons.push(new Person('Ivan', 'Ivanov', 5));
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  onAddPerson(person: Person) {
+    if (this.persons.length > 0){
+      let newId = this.persons[this.persons.length - 1].id + 1;
+      person.id = newId;
+      this.persons.push(person);
+    }
+    else {
+      person.id = 1;
+      this.persons.push(person);
+    }
+  }
+
+  test(even) {
+    console.log(event);
+  }
+
+  
+  onDeletePerson(del_id: number) {
+    this.persons.splice(this.persons.indexOf(this.persons.find(index => index.id == del_id)), 1);
+  }
+
+  onEditPerson(person: Person) {
+    Object.assign (this.persons.find(index => index.id === person.id), person);
+  }
+
+}
+
